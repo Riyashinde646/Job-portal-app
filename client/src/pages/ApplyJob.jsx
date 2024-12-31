2cfa1ce3da45102f7dcdee3b67eca5a2657715db
Riyashinde646 <shinderiya23jan@gmail.com>
2024-12-31 12:41:04 +0530

    31/12/24
    fetching jobs

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const ApplyJob= () =>{
 
   const{id}=useParams()
 
   const[JobData,setJobData]=useState(null)
+
+  const{jobs}= useContext(AppContext)
+
+  const fetchJob=async ()=>{
+    const data=jobs.filter(job=>job._id===id)
+    if (data.length!==0) {
+      setJobData(data[0])
+      console.log(data [0]);
+    }
+
+  }
 
   return(
     <div>
