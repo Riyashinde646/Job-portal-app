c7421b5ed5d454f02cf94a03d793e8fdcf13b877
Riyashinde646 <shinderiya23jan@gmail.com>
2025-01-02 00:26:00 +0530

    2/1/25
    dddddddddd

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -38,8 +38,8 @@ const ApplyJob= () =>{
     <div className="min-h-screen flex flex-col py-10 container px-4 2xl:px-20 mx-auto">
       <div className="bg-white text-black rounded-lg w-ful">
          <div className="flex justify-center md:justify-between flex-wrap gap-8 px-14 py-20 mb-6 bg-sky-50 border border-sky-400 rounded-xl">
-          <div>
-            <img src={JobData.companyId.image} alt="" />
+          <div className="flex flex-col md:flex-row items-center">
+            <img className="h-24 bg-white rounded-lg p-4 mr-4 max:md:mb-4 border" src={JobData.companyId.image} alt="" />
             <div>
               <h1>{JobData.title}</h1>
               <div>
