45b204afc8619953d91c02b49819e6127eb354ff
Riyashinde646 <shinderiya23jan@gmail.com>
2025-01-02 00:14:38 +0530

    2/1/25
    bbbbbbbb

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
-import { jobsData } from "../assets/assets";
+import { assets} from "../assets/assets";
 import Loading from "../components/Loading";
 import Navbar from "../components/Navbar";
+import kconvert from 'k-convert';
+import moment from 'moment';
 
 const ApplyJob= () =>{
 
@@ -32,6 +34,42 @@ const ApplyJob= () =>{
   return JobData ?(
     <>
     <Navbar/>
+
+    <div className="min-h-screen flex flex-col py-10 container px-4 2xl:px-20 mx-auto">
+      <div className="bg-white text-black rounded-lg w-ful">
+         <div className="flex justify-center md:justify-between flex-wrap gap-8 px-14 py-20 mb-6 bg-sky-50 border border-sky-400 rounded-xl">
+          <div>
+            <img src={JobData.companyId.image} alt="" />
+            <div>
+              <h1>{JobData.title}</h1>
+              <div>
+                <span>
+                  <img src={assets.suitcase_icon} alt="" />
+                  {JobData.companyId.name}
+                </span>
+                <span>
+                  <img src={assets.location_icon} alt="" />
+                  {JobData.location}
+                </span>
+                <span>
+                  <img src={assets.person_icon} alt="" />
+                  {JobData.level}
+                </span>
+                <span>
+                  <img src={assets.money_icon} alt="" />
+                 CTC: {kconvert.convertTo(JobData.salary)}
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <button>Apply Now</button>
+          <p>Posted {moment(JobData.date).fromNow()}</p>
+
+         </div>
+      </div>
+    </div>
+
     </>
   ):(
     <Loading/>
