5a10357b5eb87d52fb1428a9d5dadb2b1e79303d
Riyashinde646 <shinderiya23jan@gmail.com>
2025-01-06 23:55:04 +0530

    6/2/25
    resume update

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 
 const Applications= () =>{
-
+ 
+const[isEdit,setIsEdit]=useState(false)
 
   return(
     <>
       <Navbar/>
-      <div>
-        <h2>Your Resume</h2>
-        <div>
+      <div className="container px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10">
+        <h2 className="text-xl font-semibold">Your Resume</h2>
+        <div className="flex gap-2 mb-6 mt-3">
+        {
+          isEdit?<>
           
+          </>
+          :<div>
+           <a href="">
+            Resume
+           </a>
+           <button>
+            Edit
+           </button>
+          </div>
+        }
         </div>
       </div>
     </>
