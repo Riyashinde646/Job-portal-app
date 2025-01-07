7942d4b7a7710df6ddb538bf95c2240130596bb4
Riyashinde646 <shinderiya23jan@gmail.com>
2025-01-07 12:28:26 +0530

    7/2/25
    resume update

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
-import { assets } from "../assets/assets";
+import { assets, jobsApplied } from "../assets/assets";
 
 const Applications= () =>{
  
@@ -20,7 +20,7 @@ const[resume,setResume]=useState(null)
             <input id="resumeUpload" onChange={e=>setResume(e.target.files[0])} accept="application/pdf" type="file" hidden/>
             <img src={assets.profile_upload_icon} alt="" />
           </label>
-          <button>Save</button>
+          <button onClick={e=>setIsEdit(false)} className="bg-blue-100 border border-green-400 rounded-lg px-4 py-2">Save</button>
           </>
           :<div className="flex gap-2">
            <a className="bg-blue-100 text-blue-600 px-4 py-2 rounded-lg" href="">
@@ -32,6 +32,29 @@ const[resume,setResume]=useState(null)
           </div>
         }
         </div>
+        <h2>Jobs Applied</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Job Title</th>
+              <th>Location</th>
+              <th>Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobsApplied.map((job,index)=> job.jobId ? (
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            ):(null))}
+          </tbody>
+        </table>
       </div>
     </>
   )
