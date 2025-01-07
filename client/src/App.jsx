c02ca45280c5db8d55fdba3208edd883be885488
Riyashinde646 <shinderiya23jan@gmail.com>
2025-01-08 00:43:37 +0530

    bbb
    bbbbb

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import {Route,Routes}from 'react-router-dom'
 import Home from "./pages/Home";
 import ApplyJob from "./pages/ApplyJob";
 import Applications from "./pages/Applications";
+import RecruiterLogin from "./components/RecruiterLogin";
 
 
 
 const App= () =>{
   return(
     <div>
+      <RecruiterLogin/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path='/apply-job/:id' element={<ApplyJob />} />
---
+++ client/src/components/RecruiterLogin.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { use } from "react";
+
+
+const RecruiterLogin = () =>{
+
+    const[state,setState] = useState('Login')
+    const[name,setName] = useState('')
+    const[password,setPassword] = useState('')
+    const[email,setEmail] = useState('')
+
+    const[image,setImage] = useState(false)
+
+    const
+
+return(
+    <div>
+
+    </div>
+)
+
+
+
+}
+
+export default RecruiterLogin
