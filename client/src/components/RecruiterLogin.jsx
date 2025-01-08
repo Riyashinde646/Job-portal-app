e5ac184a4677d1010841283772b16f518bed98b9
Riyashinde646 <shinderiya23jan@gmail.com>
2025-01-08 16:56:58 +0530

    8/1/25
    recruiter login page

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -14,28 +14,51 @@ const RecruiterLogin = () =>{
 
     const[isTextDatasubmited,setIsTextDatasubmited] = useState(false)
 
+    const onSubmitHandler
+
 return(
     <div className="absolute top-0 left-0 right-0 bottom-0 z-10 backdrop:blur-sm bg-black/30 flex justify-center items-center">
    <form className="relative bg-white p-10 rounded-xl text-slate-500">
     <h1 className="text-center text-2xl text-neutral-700 font-medium">Recruiter {state}</h1>
-    <p>Welcome back! Please sign in to continue</p>
-    <>
-    <div>
-        <img src={assets.person_icon} alt="" />
-        <input onChange={e=>setName(e.target.value)} value={name} type="text"  placeholder="Company Name" required/> 
-    </div>
-    <div>
-        <img src={assets.email_icon} alt="" />
-        <input onChange={e=>setEmail(e.target.value)} value={email} type="email"  placeholder="Email Id" required/> 
-    </div>
-    <div>
-        <img src={assets.lock_icon} alt="" />
-        <input onChange={e=>setPassword(e.target.value)} value={password} type="password"  placeholder="Password" required/> 
-    </div>
+    <p className="text-sm">Welcome back! Please sign in to continue</p>
+    {state === "Sign Up" && isTextDatasubmited
+    ? <>
+    
+    
     </>
-    <button>
+    : <>
+      
+    {state!=='Login' && (
+      <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+      <img src={assets.person_icon} alt="" />
+      <input className="outline-none text-sm" onChange={e=>setName(e.target.value)} value={name} type="text"  placeholder="Company Name" required/> 
+  </div>
+
+    )}
+
+  <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+      <img src={assets.email_icon} alt="" />
+      <input className="outline-none text-sm" onChange={e=>setEmail(e.target.value)} value={email} type="email"  placeholder="Email Id" required/> 
+  </div>
+  <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5"> 
+      <img src={assets.lock_icon} alt="" />
+      <input className="outline-none text-sm" onChange={e=>setPassword(e.target.value)} value={password} type="password"  placeholder="Password" required/> 
+  </div>
+   
+
+  </> }
+
+  <p className="text-sm text-blue-600 my-4 cursor-pointer">Forgot password?</p>
+    
+    <button className="bg-blue-600 w-full text-white py-2 rounded-full">
         {state==='Login' ? 'login':'create account'}
     </button>
+     {
+        state==='Login'
+        ? <p className="mt-5 text-center">Don't have an account? <span className="text-blue-600 cursor-pointer"  onClick={()=>setState("Sign up")}>Sign up</span></p>
+        :  <p className="mt-5 text-center"> Already have an account? <span className="text-blue-600 cursor-pointer" onClick={()=>setState("Login")}>Login</span></p>
+     }
+    
    </form>
     </div>
 )
