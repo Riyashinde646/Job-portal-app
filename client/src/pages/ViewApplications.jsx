95f0b6f687e76ac729c3265a366a4f21c6a5285c
Riyashinde646 <shinderiya23jan@gmail.com>
2025-01-12 23:21:27 +0530

    12/1/25
    view applications

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { viewApplicationsPageData } from "../assets/assets";
 
 const ViewApplications =() => {
     return(
-        <div>ViewApplications</div>
+        <div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th> User name</th>
+                            <th>Job Title</th>
+                            <th>Location</th>
+                            <th>Resume</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {viewApplicationsPageData.map(()=>(
+                            <tr></tr>
+            
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
